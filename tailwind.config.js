1c4c446b83983fe10db3780280dde5289308ef71
rhistoba <fujitakeisound@gmail.com>
2019-09-23 11:36:31 +0900

    見た目を修正中

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   theme: {
     extend: {}
   },
-  variants: {},
+  variants: {
+    borderWidth: ['responsive', 'first', 'last', 'even', 'odd', 'hover', 'focus']
+  },
   plugins: []
 }
