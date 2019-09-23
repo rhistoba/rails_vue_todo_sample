711d0aeec35c5e6b2995b9a8088f0bb809ac6eab
rhistoba <fujitakeisound@gmail.com>
2019-09-23 10:40:56 +0900

    Todoを画面で追加できるようにした

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,25 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
+    @new_todo = Todo.new
+  end
+
+  def create
+    @new_todo = Todo.new(todo_params)
+    if @new_todo.save
+      redirect_to root_path
+    else
+      @todos = Todo.all
+      render :index
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(
+      :title,
+      :deadline
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'todos#index'
+  post 'todos', to: 'todos#create'
 end
